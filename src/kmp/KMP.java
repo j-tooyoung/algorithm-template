fc1f6589d8a70087f019e880883dccc7c3da7c50
j-tooyoung <arnoldtooyoung@hotmail.com>
2020-11-04 18:06:23 +0800

    submit

--- src/kmp/KMP.java
+++ src/kmp/KMP.java
@@ -2,6 +2,7 @@ package kmp;
 
 /**
  * 状态机
+ * https://zhuanlan.zhihu.com/p/83334559
  */
 public class KMP {
 
@@ -45,7 +46,30 @@ public class KMP {
         this.pat = pat;
         // 通过 pat 构建 dp 数组
         // 需要 O(M) 时间
+        this.pat = pat;
+        int M = pat.length();
+        // dp[状态][字符] = 下个状态
+        dp = new int[M][256];
+        // BASE CASE
+        dp[0][pat.charAt(0)] = 1;
 
+        // 影子状态 X 初始为 0
+//        前者是在 pat 中匹配 pat[1..end]，状态 X 总是落后状态 j 一个状态，与 j 具有最长的相同前缀。
+        int X = 0;
+        // 当前状态 j 从 1 开始
+        for (int j = 0; j < M; j++) {
+            for (int c = 0; c < 256; c++) {
+                // 状态匹配，推进
+                if (pat.charAt(j) == c) {
+                    dp[j][c] = j + 1;
+                } else {
+                    // 回退
+                    dp[j][c] = dp[X][c];
+                }
+            }
+            // 更新影子状态
+            X = dp[X][pat.charAt(j)];
+        }
     }
 
     public int search(String txt) {
